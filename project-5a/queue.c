3a7ee2cd180df281de589970f48d893d3489f113
Benyamain <yacoobbenyamain@gmail.com>
2024-04-01 09:11:39 -0400

    setting up for project 5a

---
+++ project-5a/queue.c
@@ -0,0 +1,38 @@
+/* Code for enqueueing, dequeueing, and printing linked list of PROCs  */
+
+#include "Llibc/Llibc.h"
+#include "type.h"
+#include "queue.h"
+
+/* Enqueueing routine for the scheduler */
+int
+enqueue(PROC **queue, PROC *p)
+{
+    /* Enter p into queue by priority; FIFO (FCFS) if same prioirty */
+
+    /* Add YOUR CODE here */
+
+}
+
+/* Dequeueing routine for the scheduler */
+PROC
+*dequeue(PROC **queue)
+{
+    /* Remove and return first PROC from queue */
+
+    /* Add YOUR CODE here */
+
+}
+
+/* Print queue, starting at given PROC */
+int
+printList(char *name, PROC *p)
+{
+    Lprintf("%s = ", name);
+
+    /* Print linked list of PROCs as:  [pid priority]-> ... ->NULL */
+
+    /* Add YOUR CODE here */
+
+}
+
