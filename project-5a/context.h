bff730dc64b94be8639a377f4b19f454f381dbc4
Benyamain <yacoobbenyamain@gmail.com>
2024-04-05 10:33:44 -0400

    fix??

--- project-5a/context.h
+++
@@ -1,5 +0,0 @@
-/**
-    Offset to context area within PCB struct PROC (see type.h).
-    Separated here from type.h for use by the assembler.
- **/
-#define OFF_CTX_PCB 40      /* Given PROC *p, this == p - &(p->uctx[0]) */
--- project-5a/queue.h
+++
@@ -1,6 +0,0 @@
-/* Queue-management functions defined in queue.c */
-
-int enqueue(PROC **, PROC *);       /* Scheduler enqueueing routine */
-PROC *dequeue(PROC **);             /* Scheduler dequeueing routine */
-int printList(char *, PROC *);      /* Print queue, starting at given PROC */
-
